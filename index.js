4999fb9e1ba236899e375777c3da9c48647ac9f2
yuki200adi <144140894+yuki200adi@users.noreply.github.com>
2023-10-02 11:19:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var noOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i =0 ;i<noOfDrums ; i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function()
+
+{
+
+
+    alert("I got clicked!");
+});
+}
